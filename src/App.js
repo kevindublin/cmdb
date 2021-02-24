824e9e2272d883fc10205456f7070642b2b7e8ad
Kevin Dublin <kevin@kevindublin.com>
2021-02-24 04:07:49 -0800

    successfully fetching and console logging data on search

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
 import './App.css';
+import './main.css';
+import banner from './banner.png'
+import getMoviesByNameB from './utils.js';
+
+let state = {
+    query: "Batman",
+  }
+
+  const onSearchChange = (ev) => {
+    let value = ev.target.value;
+    state = {...state,
+            query: value,};
+  }
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img className="headerImage" src={banner} />
+        <div className="container h-100">
+          <div className="row h-100 align-text-bottom" id="main-header">
+            <div className="col-lg-12">
+              <h1 className="display-4 text-white mt-5">CMDB</h1>
+              <p className="lead mt-5 text-50">Because you want to search movies differently</p>
+            </div>
+          </div>
+        </div>
+
+
+      <div className="container">
+
+        <div className="row">
+          
+            <h2>Search</h2>
+            <hr />
+              <div className="form-group row">
+                <div className="col-sm-10">
+                  <input className="form-control" 
+                    id="searchQuery" 
+                    placeholder="search..."
+                    onChange={onSearchChange} />
+                </div>
+              </div>
+              <button className="btn btn-primary" id="searchButton" onClick={() => getMoviesByNameB(state.query)}>Search</button>
+        </div>
+        
+        <h2>Search Results</h2>
+        <hr />
+        <div className="row">
+          <div className="col-md-4 mb-5">
+            <div className="card" style={{width: "18rem"}}>
+              <img className="card-img-top" src="..." alt="poster" />
+              <div className="card-body">
+                <p className="card-text">Movie Title</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4 mb-5">
+            <div className="card" style={{width: "18rem"}}>
+              <img className="card-img-top" src="..." alt="poster" />
+              <div className="card-body">
+                <p className="card-text">Movie Title</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4 mb-5">
+            <div className="card" style={{width: "18rem"}}>
+              <img className="card-img-top" src="..." alt="poster" />
+              <div className="card-body">
+                <p className="card-text">Movie Title</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+      <footer className="py-5 bg-dark">
+        <div className="container">
+          <p className="m-0 text-center text-white">This is a simple movie database.</p>
+        </div>
+      </footer>
     </div>
   );
 }
---
+++ src/utils.js
@@ -0,0 +1,45 @@
+
+
+const getMoviesByName = (query) => {
+	let apiKey = `${process.env.REACT_APP_OMDB_KEY}`
+	let baseUrl = "https://www.omdbapi.com/"
+	let endpoint = "?s"
+	let url = `${baseUrl}?apikey=${apiKey}&s=${query}`
+	console.log(url);
+	fetch(url)
+		.then(response => response.json())
+		.then(data => {
+			console.log("result returned:", data);
+		})
+}
+
+
+const getMovieDetailsById = (ev) => {
+	let apiKey = `${process.env.REACT_APP_OMDB_KEY}`
+	fetch("https://www.omdbapi.com/?i=" + ev + "&apikey=" + apiKey)
+		.then(response => response.json())
+		.then(data => {
+			console.log("result returned:", data);
+		})
+}
+
+// getMovieDetailsById("tt0372784");
+
+
+const getMoviesByNameB = async (search) => {
+	
+	const baseUrl = "https://www.omdbapi.com/"
+	const apiKey = `${process.env.REACT_APP_OMDB_KEY}`
+	const url = `${baseUrl}/?apikey=${apiKey}&s=${search}`
+
+	const res = await fetch(url);
+	const data = await res.json();
+	console.log(data);
+	return data;
+}
+
+// getMoviesByNameB('batman')
+//	.then((data) => console.log(data))
+
+
+export default getMoviesByNameB;
